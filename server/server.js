6146f4d35d95efffd10441702b199bbbfe73dd19
Bradley <bg9892@gmail.com>
2020-04-23 21:53:42 -0500

    added epress and socket.io and made a seperate client and server folder

--- server/server.js
+++ server/server.js
@@ -1,16 +1,47 @@
-const app = require('express');
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
 
-server.listen(80);
+const router = require('./router');
+const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router);
 
 io.on('connection', (socket) => {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', (data) => {
-    console.log(data);
+  socket.on('join', ({ name, room }, cb) => {
+    
+    const { error, user } = ''; // TODO: add user in here
+
+    if (error) return cb(error);
+    
+    socket.join(user.room);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+    cb()
   });
-});
+
+  socket.on('sendMessage', (message, cb) => {
+    const user = ''; // TODO: get user in here
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    cb();
+  });
+
+  socket.on('disconnect', () => {
+    const user = ''; // TODO: get user in here
+
+    if (user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+    }
+  });
+
+})
+
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`));
