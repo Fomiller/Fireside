b84e84825c3b5eb76353f9b477984ca6b289bb2d
Forrest <Forrestmillerj@gmail.com>
2020-04-25 17:59:36 -0500

    added export for 404/NoMatch page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,3 +3,4 @@ export {default as SignIn} from './signin';
 export {default as Profile} from './profile';
 export {default as SignUp} from './signup';
 export {default as User} from './user';
+export {default as NoMatch} from './nomatch';
