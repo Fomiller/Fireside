582a92c3af948d7743f70645cce5898556d9b209
Forrest <Forrestmillerj@gmail.com>
2020-04-26 14:02:37 -0500

    Wrapped app in global context.

--- client/src/App.js
+++ client/src/App.js
@@ -5,37 +5,40 @@ import {Home, SignIn, SignUp, Profile, User, NoMatch} from './pages';
 import StickyFooter from './components/footer';
 import NavbarDrawer from './components/navigation';
 import ApplicationTheme from './utils/theme';
+import AppProvider from './utils/GlobalContext';
 
 function App() {
   return (
-    <ThemeProvider theme={ApplicationTheme}>
-      <Router>
-        <Switch>
-          <Route exact path='/signin'>
-            <SignIn/>
-          </Route>
-          <Route exact path='/signup'>
-            <SignUp/>
-          </Route>
-          <Route exact path={['/','/home']}>
-            <NavbarDrawer/>
-            <Home/>
-          </Route>
-          <Route exact path='/profile'>
-            <NavbarDrawer/>
-            <Profile/>
-          </Route>
-          <Route exact path='/user/:id'>
-            <NavbarDrawer/>
-            <User/>
-          </Route>
-          <Route exact path='*'>
-          <NoMatch/>
-          </Route>
-        </Switch>
-        <StickyFooter/>
-      </Router>
-    </ThemeProvider>
+    <AppProvider>
+      <ThemeProvider theme={ApplicationTheme}>
+        <Router>
+          <Switch>
+            <Route exact path='/signin'>
+              <SignIn/>
+            </Route>
+            <Route exact path='/signup'>
+              <SignUp/>
+            </Route>
+            <Route exact path={['/','/home']}>
+              <NavbarDrawer/>
+              <Home/>
+            </Route>
+            <Route exact path='/profile'>
+              <NavbarDrawer/>
+              <Profile/>
+            </Route>
+            <Route exact path='/user/:id'>
+              <NavbarDrawer/>
+              <User/>
+            </Route>
+            <Route exact path='*'>
+            <NoMatch/>
+            </Route>
+          </Switch>
+          <StickyFooter/>
+        </Router>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
 
