61b196d0f73df90705777b9f4fe3bf48fa26d3f3
Forrest <Forrestmillerj@gmail.com>
2020-04-26 14:07:56 -0500

    Imported state to test functunality

--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, {useContext} from "react";
+import {useAppContext} from '../utils/GlobalContext';
 
 export default function User() {
+  const [state, dispatch] = useAppContext();
+  console.log('STATE: ', state);
+
   return(
     <div>User</div>
   )
