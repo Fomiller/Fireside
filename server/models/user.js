cbd23dcb066edaa31d22661921be405156bd222a
Bradley <bg9892@gmail.com>
2020-04-28 16:07:13 -0500

    added passportLocalMongoose to userSchema

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
     firstName: {
@@ -34,6 +35,8 @@ const UserSchema = new Schema({
 }, { toJSON: { virtuals: true } 
 });
 
+// Middleware to hash passwords
+UserSchema.plugin(passportLocalMongoose);
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
