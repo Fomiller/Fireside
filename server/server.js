eddff779b7f15e36a14e83ec8d9c7723c6b9acd4
Bradley <bg9892@gmail.com>
2020-04-28 16:30:00 -0500

    added requires for socketIO and passport

--- server/server.js
+++ server/server.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
+const passport = require('./config/passport');
+
 // Socket.io setup
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const http = require('http');
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
+
 // require Mongoose
 const mongoose = require("mongoose");
-
-// Dont know what these are used for
-const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
-// dont know what this is
-// const config = require("./config/key");
-
 
 // Connecting to mongoose.
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/firesideDB", { useNewUrlParser: true });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// dont know what this is used for
-// app.use(cookieParser());
+// Setup app to use sessions to keep track of user's login status.
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 const { Chat } = require("./models/Chat");
 
@@ -51,10 +51,9 @@ io.on("connection", socket => {
       }
     })
    })
-
 })
 
-
+// Dont know what this does?
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
@@ -71,8 +70,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000
 
-server.listen(port, () => {
-  console.log(`Server Running at ${port}`)
-});
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`Server Running at ${PORT}`));
