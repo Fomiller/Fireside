ca82d46ec0baac9957fe0ab6bf3a7a56ce715fdf
Forrest <Forrestmillerj@gmail.com>
2020-04-28 11:18:06 -0500

    updated Mongoose connection. Added comments to imports

--- server/server.js
+++ server/server.js
@@ -8,9 +8,7 @@ const io = require("socket.io")(server);
 const config = require("./config/key");
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/firesideDB", { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
