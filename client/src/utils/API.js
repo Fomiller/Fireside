4b684c62da34a288f30335491013aeefee5b9dc1
Forrest <Forrestmillerj@gmail.com>
2020-04-29 12:16:57 -0500

    Added redirect after login on the client side

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
 
+
 export const createUser = (data) => {
   axios.post('/api/users', data)
+}
+
+export const login = (data) => {
+  axios.post('/api/login', data)
+  .then( res => {
+    console.log(res);
+    console.log("USERNAME: ",res.data.username)
+    const username = res.data.username 
+    if (res.status === 200 && window){ 
+      window.location.href = `/user/${username}`; 
+      // or <Redrect to="/thankyou" /> if you are using react-router
+    }
+  })
+  .catch(err => {
+    console.log("LOGIN CLIENT SIDE ERROR: ", err)
+  })
 }
