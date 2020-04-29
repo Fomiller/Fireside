fdb77464bbaa6e1c30036093715e676cd4215b74
Forrest <Forrestmillerj@gmail.com>
2020-04-28 21:21:47 -0500

    styled nomatch page.

--- client/src/pages/nomatch.js
+++ client/src/pages/nomatch.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import { Typography } from '@material-ui/core';
+import { useTheme } from '@material-ui/styles';
 
 export default function NoMatch() {
+
   return (
-    <div>
-      <h1>404 PAGE NOT FOUND</h1>
-    </div>
+    <Container>
+      <Typography align='center'>
+        <Typography align='center' display='inline' color='secondary' variant='h1'>
+          404 
+        </Typography>
+        <Typography align='center' display='inline' color='primary' variant='h1'>
+          PAGE
+        </Typography>
+        <Typography align='center' display='inline' color='textPrimary' variant='h1'>
+          NOT
+        </Typography>
+        <Typography align='center' display='inline' color='secondary' variant='h1'>
+          FOUND
+        </Typography>
+      </Typography>
+      
+    </Container>
   )
 }
