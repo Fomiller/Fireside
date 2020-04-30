a78f7c6851ae610d2f648b7ce1ec1beada8e62b8
Forrest <Forrestmillerj@gmail.com>
2020-04-29 22:04:38 -0500

    styling

--- client/src/components/join/index.js
+++ client/src/components/join/index.js
@@ -4,27 +4,49 @@ import { useAppContext } from '../../utils/GlobalContext';
 import Button from '@material-ui/core/Button';
 import { Box } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
+import {Grid} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  notchedOutline: {
+    borderWidth: "1px",
+    borderColor: "yellow !important",
+  },
+  roomInput: {
+    marginBottom: '50px',
+  }
+}));
 
 export default function Join() {
   const [state, dispatch] = useAppContext()
   const [room, setRoom] = useState('');
+  const classes = useStyles();
 
   return (
-        <Box>
-          <TextField
-          id="outlined-basic"
-          label="Enter Room"
-          variant="outlined"
-          onChange={(event) => setRoom(event.target.value)}
-          />
-          <Button
-          component={Link}
-          variant="outlined"
-          color='secondary'
-          onClick={event => (!room) ? event.preventDefault() : null}
-          to={`/chat?name=${state.user.username}&room=${room}`}>
-            Join Chat Room
-          </Button>
-        </Box>
+          <Grid item xs={12}>
+            <TextField
+            id="outlined-basic"
+            label="Enter Room"
+            variant="outlined"
+            onChange={(event) => setRoom(event.target.value)}
+            fullWidth
+            className={classes.roomInput}
+            InputProps={{
+              classes: {
+                notchedOutline: classes.notchedOutline
+              }
+            }}
+            />
+            <Button
+            fullWidth
+            component={Link}
+            variant="outlined"
+            color='secondary'
+            onClick={event => (!room) ? event.preventDefault() : null}
+            to={`/chat?name=${state.user.username}&room=${room}`}>
+              Join Chat Room
+            </Button>
+          </Grid>
   );
 }
