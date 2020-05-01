9f835890e2c9542304ab7fd243ea107580112b6a
Bradley <bg9892@gmail.com>
2020-04-30 23:30:29 -0500

    added route to retrieved stored messages

--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -4,98 +4,81 @@ const router = express.Router();
 const User = require('../models/user');
 const passport = require('../config/passport');
 
-
-  // gets ALL USERS
-  router.get("/api/users", (req, res) => {
-    db.User.find({})
-      .then(User => {
-        res.json(User);
-      })
-      .catch(err => {
-        res.json(err)
-      });
-  });
-
-  //Route for registering new user
-  router.post('/api/users', (req, res) => {
-    Users = new User(req.body);
-
-    User.register(Users, req.body.password, function (err, user) {
-      if (err) {
-        res.json({ success: false, message: "Your account could not be saved. Error: ", err })
-      } else {
-        res.json({ success: true, message: "Your account has been saved" })
-      }
+// Gets all users.
+router.get("/api/users", (req, res) => {
+  db.User.find({})
+    .then(User => {
+      res.json(User);
+    })
+    .catch(err => {
+      res.json(err)
     });
-  });
+});
 
-  // Route for loging in
-  router.post('/api/login',passport.authenticate('local'), async (req, res) => {
-    if (req.user) {
-      const user = await db.User.findOne({ username: req.user.username })
-      console.log('SUCCESS!!!');
-      return res.send(user)
+// Route for registering new user.
+router.post('/api/users', (req, res) => {
+  Users = new User(req.body);
+
+  User.register(Users, req.body.password, function (err, user) {
+    if (err) {
+      res.json({ success: false, message: "Your account could not be saved. Error: ", err })
     } else {
-      console.log('username or password incorrect');
-      res.json({ success: false, message: 'username or password incorrect' })         
+      res.json({ success: true, message: "Your account has been saved" })
     }
   });
+});
 
-  router.get('/api/loggedinuser', (req,res) => {
-    console.log("SERVER",req.user);
-    if (req.user) {
-      return res.send(req.user);
-    }
-    return res.send(null);
-  });
+// Route for loging in.
+router.post('/api/login', passport.authenticate('local'), async (req, res) => {
+  if (req.user) {
+    const user = await db.User.findOne({ username: req.user.username })
+    console.log('SUCCESS!!!');
+    return res.send(user)
+  } else {
+    console.log('username or password incorrect');
+    res.json({ success: false, message: 'username or password incorrect' })
+  }
+});
 
-  // get a single USER by id
-  router.get('/api/users/:id', (req, res) => {
-    db.User.findOne({ _id: req.params.id })
-      .then(User => {
-        res.json(User)
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+router.get('/api/loggedinuser', (req, res) => {
+  if (req.user) {
+    return res.send(req.user);
+  }
+  return res.send(null);
+});
 
-  // Update a USER
-  router.put('/api/users/:id', (req, res) => {
-    db.User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-      .then(User =>
-        res.json(User)
-      )
-      .catch(err => {
-        // if an error occurs send the error to the client
-        res.json(err);
-      });
-  });
+// Get a single USER by id.
+router.get('/api/users/:id', (req, res) => {
+  db.User.findOne({ _id: req.params.id })
+    .then(User => {
+      res.json(User)
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
+// Update a USER.
+router.put('/api/users/:id', (req, res) => {
+  db.User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then(User =>
+      res.json(User)
+    )
+    .catch(err => {
+      // If an error occurs send the error to the client.
+      res.json(err);
+    });
+});
 
+// Get saved messages.
+router.get('/api/messages/:room?', (req, res) => {
+  db.Chat.find({ room: req.params.room })
+    .then(chat => {
+      res.send(chat);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
-  // Routes to add and get friends for a user
-
-  // router.get('/api/users/friends', (req, res) => {
-  //   db.User.find({})
-  //   .populate('friends')
-  //     .then(dbWorkout => {
-  //       res.json(dbWorkout);
-  //     })
-  //     .catch(err => {
-  //       res.json(err)
-  //     });
-  // });
-
-  // router.put('/api/addfriend/:id', (req, res) => {
-  //   db.User.findOneAndUpdate({ _id: req.params.id }, { $push: { friends: req.body } }, { new: true })
-  //   .then(User => 
-  //     res.json(User)
-  //   )
-  //   .catch(err => {
-  //     // if an error occurs send the error to the client
-  //     res.json(err);
-  //   });
-  // });
-
-  module.exports = router;
+module.exports = router;
