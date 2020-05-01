53694244376104d0c400ef2bc988fc1394ceae6e
Bradley <bg9892@gmail.com>
2020-04-30 23:47:00 -0500

    added api call to get stored messages

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,41 +1,53 @@
 import axios from 'axios';
 
-
 export const createUser = async (data) => {
   try {
-    const res = await axios.post('/api/users', data)
-    if (res.status === 200 && window){ 
+    const res = await axios.post('/api/users', data);
+    if (res.status === 200 && window) {
       return res;
     }
   }
   catch (err) {
-    return err
+    return err;
   }
-}
+};
 
 export const login = async (data) => {
-  try{
-      const res = await axios.post('/api/login', data);
-      if (res.status === 200 && window){ 
-        return res.data;
-      }
-      return null;
+  try {
+    const res = await axios.post('/api/login', data);
+    if (res.status === 200 && window) {
+      return res.data;
     }
-    catch (err) {
-      console.log("LOGIN CLIENT SIDE ERROR: ", err)
-      
-    }
-}
+    return null;
+  }
+  catch (err) {
+    console.log("LOGIN CLIENT SIDE ERROR: ", err);
+  }
+};
 
 export const getLoggedInUser = async () => {
-  try{
+  try {
     const res = await axios.get('/api/loggedinuser');
     if (res.data) {
       return res.data;
     }
     return null;
   }
-  catch(err){
-    console.log("GET LOGGED IN USER: ",err)
+  catch (err) {
+    console.log("GET LOGGED IN USER: ", err);
   }
-}
+};
+
+export const getMessages = async (room) => {
+  try {
+    const res = await axios.get('/api/messages/' + room);
+    if (res.data) {
+      console.log('API: ',res.data);
+      return res.data;
+    }
+    return null;
+  } 
+  catch (err) {
+    console.log("GET MESSAGES ERROR: ", err);
+  }
+};
