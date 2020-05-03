7c8cd36170b85592c7a332d199df5bb808886e77
Forrest <Forrestmillerj@gmail.com>
2020-05-03 13:17:51 -0500

    added back in the input room field and join button from the join component. the spacing is the general reason why.

--- client/src/components/UserRight/index.js
+++ client/src/components/UserRight/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Grid from '@material-ui/core/Grid';
-import { Typography, Button } from '@material-ui/core';
+import { Typography, Button, TextField } from '@material-ui/core';
 import {useAppContext} from '../../utils/GlobalContext';
 import Join from '../join';
 import { logout } from '../../utils/API';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  notchedOutline: {
+    borderWidth: "1px",
+    borderColor: "yellow !important",
+  },
+  roomInput: {
+    marginBottom: '50px',
+  },
+  joinBtn: {
+    borderWidth: '1px',
+  },
+  logoutBtn: {
+    color:theme.palette.error.dark,
+    borderColor: theme.palette.error.dark,
+  }
+}));
 
 export default function UserRight() {
   const [state, dispatch] = useAppContext();
+  const roomRef = useRef(null);
+  const classes = useStyles();
+
+  const CustomLink = (props) => <Link  to={`/chat?name=${state.user.username}&room=${state.room}`} {...props} />
 
 
   return(
@@ -27,10 +51,30 @@ export default function UserRight() {
         </Typography>
       </Grid>
       <Grid item xs={12}>
-        <Join/>
+      <TextField
+        inputRef={roomRef}
+        id="outlined-basic"
+        label="Enter Room"
+        variant="outlined"
+        onChange={() => dispatch({type:'SET_ROOM', payload:roomRef.current.value})}
+        fullWidth
+        InputProps={{
+          classes: {
+            notchedOutline: classes.notchedOutline
+          }
+        }}
+        />
       </Grid>
       <Grid item xs={12}>
-        <Button onClick={() => dispatch({type:"LOGOUT"})}></Button>
+      <Button
+        fullWidth
+        variant="outlined"
+        component={CustomLink}
+        color='secondary'
+        onClick={event => (!state.room) ? event.preventDefault() : null}
+        >
+        Join Chat Room
+        </Button>
       </Grid>
     </Grid>
   )
