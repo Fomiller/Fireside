2a777b53659e2789e76202260d8420514e7fd218
Forrest <Forrestmillerj@gmail.com>
2020-05-03 13:40:18 -0500

    updated

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -43,10 +43,9 @@ export default function StickyFooter() {
 
   return (
     <div className={classes.root}>
-
       <footer className={classes.footer}>
         <Container maxWidth="sm">
-          {/* <Typography variant="body1">My sticky footer can be found here.</Typography> */}
+          <Typography variant="body1">My sticky footer can be found here.</Typography>
           <Copyright />
         </Container>
       </footer>
